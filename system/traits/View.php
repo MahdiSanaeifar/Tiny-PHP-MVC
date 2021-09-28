911804de0f62c3c6840b3101c7fff4b34ac42e75
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2021-09-28 17:39:05 +0330

    tiny edit

--- system/traits/View.php
+++ system/traits/View.php
@@ -8,12 +8,12 @@ trait View
         $dir = str_replace('.', '/', $dir);
         if ($vars) {
             extract($vars);
-            $path = realpath(dirname(__FILE__) . "/../../application/view/" . $dir . ".php");
-            if (file_exists($path)) {
-                return require_once $path;
-            } else {
-                echo "this view [" . $dir . "] not exist";
-            }
+        }
+        $path = realpath(dirname(__FILE__) . "/../../application/view/" . $dir . ".php");
+        if (file_exists($path)) {
+            return require_once $path;
+        } else {
+            echo "this view [" . $dir . "] not exist";
         }
     }
 }
