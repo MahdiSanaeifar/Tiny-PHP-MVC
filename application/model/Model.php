94dec0218da2beef96f69a6eb08e78c15ab85a5d
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2021-09-29 12:45:18 +0330

    create __destruct & query function

--- application/model/Model.php
+++ application/model/Model.php
@@ -22,5 +22,44 @@ class Model
         }
     }
 
+    public function __destruct()
+    {
+        $this->closeConnection();
+    }
 
+    public function query($query, $values = null)
+    {
+        try {
+            if ($values == null) {
+                return $this->connection->query($query);
+            } else {
+                $stmt = $this->connection->prepare($query);
+                $stmt->execute($values);
+                return $stmt;
+            }
+        } catch (PDOException $e) {
+            echo "There is some problem in connection" . $e->getMessage();
+        }
+    }
+
+    public function execute($query, $values = null)
+    {
+        try {
+            if ($values == null) {
+                $this->connection->exec($query);
+            } else {
+                $stmt = $this->connection->prepare($query);
+                $stmt->execute($values);
+            }
+            return true;
+        } catch (PDOException $e) {
+            echo "There is some problem in connection" . $e->getMessage();
+            return false;
+        }
+    }
+
+    protected function closeConnection()
+    {
+        $this->connection = null;
+    }
 }
