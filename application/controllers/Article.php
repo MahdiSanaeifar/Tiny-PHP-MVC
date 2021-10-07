6ecd07e9500e4ff68bf711e64b731adf99ca20f6
MahdiSanaeifar <mahdisanaeifar@gmail.com>
2021-10-07 18:24:51 +0330

    complete Controllers/article class

--- application/controllers/Article.php
+++ application/controllers/Article.php
@@ -2,21 +2,30 @@
 
 namespace Application\Controllers;
 
+use Application\Model\Article as ArticleModel;
+use Application\Model\Category;
+
 class Article extends Controller
 {
     public function index()
     {
-
+        $article = new ArticleModel();
+        $articles = $article->all();
+        return $this->view('panel.article.index', compact('articles'));
     }
 
     public function create()
     {
-
+        $category = new Category();
+        $categories = $category->all();
+        return $this->view('panel.article.create', compact('categories'));
     }
 
     public function store()
     {
-
+        $article = new ArticleModel();
+        $article->insert($_POST);
+        return $this->redirect('article');
     }
 
     public function show($id)
@@ -26,16 +35,26 @@ class Article extends Controller
 
     public function edit($id)
     {
-
+        $categories = new Category();
+        $categories->all();
+        $ob_article = new ArticleModel();
+        $article = $ob_article->find($id);
+        return $this->view('panel.article.edit', compact('categories', 'article'));
     }
 
+    /** @noinspection PhpVoidFunctionResultUsedInspection */
     public function update($id)
     {
-
+        $article = new ArticleModel();
+        $article->update($id, $_POST);
+        return $this->redirect('article');
     }
 
+    /** @noinspection PhpVoidFunctionResultUsedInspection */
     public function destroy($id)
     {
-
+        $article = new ArticleModel();
+        $article->delete($id);
+        return $this->back();
     }
 }
